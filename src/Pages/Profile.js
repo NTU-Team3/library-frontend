0866dfa224c73310b0dd571c4487f5ebbbb2d57d
trainingresult6361 <96628430+trainingresult6361@users.noreply.github.com>
2022-07-24 18:42:44 +0800

    update layout for profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import API from "../API/APIUtils";
 import Avatar from "../components/Avatar";
 import "../Assets/Styles/Profile.css";
@@ -14,12 +15,15 @@ function Profile() {
   const pathVProfile = "/member/v-profile/" + id;
 
   const pathULoans = "/member/u-loans/";
+  const pathUReviews = "/member/u-reviews/";
+  const pathUProfile = "/member/u-profile/";
 
   const [vloans, setVLoans] = useState([]);
   const [vreservations, setVReservations] = useState([]);
   const [vhistories, setVHistories] = useState([]);
   const [vreviews, setVReviews] = useState([]);
   const [vprofile, setVProfile] = useState([]);
+  const [action, setAction] = useState(null);
 
   const getVLoans = async () => {
     const response = await API.get(pathVLoans);
@@ -80,6 +84,10 @@ function Profile() {
     window.location.reload();
   };
 
+  const callAction = (clickaction) => {
+    setAction(clickaction);
+  };
+
   const updateLoans = async (id, lid) => {
     await API.put(pathULoans, { id, lid })
       .then((response) => {
@@ -97,7 +105,7 @@ function Profile() {
     getVReservations();
     getVHistories();
     getVReviews();
-  }, []);
+  }, [action]);
 
   const { _id, name, email, location, password, profilepic } = vprofile;
 
@@ -115,180 +123,207 @@ function Profile() {
           </div>
         </div>
         {/* --------------------------------------------------------------------------------------------------------------------- */}
-        <div className="containerInfo">
-          <div className="itemInfo">
-            <div className="sectionheader">On Loan</div>
-            <div className="sectionhr"></div>
-            <div className="sectiondiv">
-              {vloans.map((vl) => (
-                <>
-                  <div key={vl.loans._id} className="populatediv">
-                    <span
-                      style={{
-                        fontSize: "18px",
-                        fontWeight: "bold",
-                        fontStyle: "italic",
-                      }}
-                    >
-                      {vl.loans.title}
-                    </span>
-                    <br />
-                    Loan date:&nbsp;&nbsp;
-                    {new Date(vl.loans.loandate).toLocaleDateString("en-GB", {
-                      weekday: "short",
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    })}
-                    <br />
-                    Due date:&nbsp;&nbsp;
-                    {new Date(vl.loans.duedate).toLocaleDateString("en-GB", {
-                      weekday: "short",
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    })}
-                    <br />
-                    <button type="button" className="button" onClick={() => updateLoans(_id, vl.loans._id)}>
-                      return
-                    </button>
-                  </div>
-                </>
-              ))}
-            </div>
-          </div>
-          <div className="itemInfo">
-            <div className="sectionheader">Reservations</div>
-            <div className="sectionhr"></div>
-            <div className="sectiondiv">
-              {vreservations.map((vrs) => (
-                <div key={vrs.reservations._id} className="populatediv">
-                  <span
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "bold",
-                      fontStyle: "italic",
-                    }}
-                  >
-                    {vrs.reservations.title}
-                  </span>
-                  <br />
-                  Status:&nbsp;&nbsp;
-                  {vrs.reservations.status}
-                  <br />
-                  Pickup By:&nbsp;&nbsp;
-                  {vrs.reservations.latestpickup == undefined
-                    ? "n/a"
-                    : new Date(vrs.reservations.latestpickup).toLocaleDateString("en-GB", {
-                        weekday: "short",
+
+        <div className="itemInfo">
+          <div className="sectionheader">On Loan</div>
+          <div className="sectionhr"></div>
+          {!vloans.length ? (
+            ""
+          ) : (
+            <table className="tablegeneric">
+              <thead>
+                <tr></tr>
+                <tr>
+                  <th className="tabletitle">Title:</th>
+                  <th>Loan date:</th>
+                  <th>Due date:</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {vloans.map((vl) => (
+                  <tr>
+                    <td>{vl.loans.title}</td>
+                    <td>
+                      {new Date(vl.loans.loandate).toLocaleDateString("en-GB", {
                         year: "numeric",
                         month: "short",
                         day: "2-digit",
                       })}
-                  <br />
-                  <button type="button" className="button" display="none">
-                    add to cart
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="itemInfo">
-            <div className="sectionheader">History</div>
-            <div className="sectionhr"></div>
-            <div className="sectiondiv">
-              {vhistories.map((vh) => (
-                <div key={vh.loans._id} className="populatediv">
-                  <span
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "bold",
-                      fontStyle: "italic",
-                    }}
-                  >
-                    {vh.loans.title}
-                  </span>
-                  <br />
-                  Return date:&nbsp;&nbsp;
-                  {new Date(vh.loans.returndate).toLocaleDateString("en-GB", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  })}
-                  <br />
-                  <br />
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="itemInfo">
-            <div className="sectionheader">Reviews</div>
-            <div className="sectionhr"></div>
-            <div className="sectiondiv">
-              {vreviews.map((vrv) => (
-                <div key={vrv.reviews._id} className="populatediv">
-                  <span
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "bold",
-                      fontStyle: "italic",
-                    }}
-                  >
-                    {vrv.reviews.title}
-                  </span>
-                  <br />
-                  Rating:&nbsp;&nbsp;
-                  {vrv.reviews.rating} ⭐
-                  <br />
-                  Review date:&nbsp;&nbsp;
-                  {new Date(vrv.reviews.reviewdate).toLocaleDateString("en-GB", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  })}
-                  <br />
-                  Comments:
-                  <br />
-                  {vrv.reviews.comments}
-                  <br />
-                  <button type="button" className="button">
-                    edit
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-          {/* --------------------------------------------------------------------------------------------------------------------- */}
+                    </td>
+                    <td>
+                      {new Date(vl.loans.duedate).toLocaleDateString("en-GB", {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit",
+                      })}
+                    </td>
+                    <td>
+                      <button type="button" className="button" onClick={() => updateLoans(_id, vl.loans._id)}>
+                        return
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        <div className="itemInfo">
+          <div className="sectionheader">Reservations</div>
+          <div className="sectionhr"></div>
+          {!vreservations.length ? (
+            ""
+          ) : (
+            <table className="tablegeneric">
+              <thead>
+                <tr></tr>
+                <tr>
+                  <th className="tabletitle">Title:</th>
+                  <th>Status:</th>
+                  <th>Pickup by:</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {vreservations.map((vrs) => (
+                  <>
+                    <tr>
+                      <td>{vrs.reservations.title}</td>
+                      <td>{vrs.reservations.status}</td>
+                      <td>
+                        {!vrs.reservations.latestpickup
+                          ? "-"
+                          : new Date(vrs.reservations.latestpickup).toLocaleDateString("en-GB", {
+                              year: "numeric",
+                              month: "short",
+                              day: "2-digit",
+                            })}
+                      </td>
+                      <td>
+                        {!vrs.reservations.latestpickup ? (
+                          "-"
+                        ) : (
+                          <button type="button" className="button" onClick={() => callAction("ecart")}>
+                            add to cart
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        <div className="itemInfo">
+          <div className="sectionheader">History</div>
+          <div className="sectionhr"></div>
+          {!vhistories.length ? (
+            ""
+          ) : (
+            <table className="tablegeneric">
+              <thead>
+                <tr></tr>
+                <tr>
+                  <th className="tabletitle">Title:</th>
+                  <th>Return date:</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {vhistories.map((vh) => (
+                  <tr>
+                    <td>{vh.loans.title}</td>
+                    <td>
+                      {new Date(vh.loans.returndate).toLocaleDateString("en-GB", {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit",
+                      })}
+                    </td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        <div className="itemInfo">
+          <div className="sectionheader">Reviews</div>
+          <div className="sectionhr"></div>
+          {!vreviews.length ? (
+            ""
+          ) : (
+            <table className="tablegeneric">
+              <thead>
+                <tr></tr>
+                <tr>
+                  <th className="tabletitle">Title:</th>
+                  <th>Review date:</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {vreviews.map((vrv) => (
+                  <tr>
+                    <td>
+                      <td>{vrv.reviews.title}</td>
+                      <td style={{ width: "5%" }}>{vrv.reviews.rating} ⭐</td>
+                      <td style={{ width: "20%" }}>{vrv.reviews.comments.slice(0, 120)} ...</td>
+                    </td>
+                    <td>
+                      {new Date(vrv.reviews.reviewdate).toLocaleDateString("en-GB", {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit",
+                      })}
+                    </td>
+                    <td>
+                      <button type="button" className="button" onClick={() => callAction("ereview")}>
+                        edit
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+        {/* --------------------------------------------------------------------------------------------------------------------- */}
 
-          <div className="itemInfo">
-            <div className="sectionheader">Profile</div>
-            <div className="sectionhr"></div>
-            <div className="sectiondiv">
-              <div className="populatediv">
-                <span
-                  style={{
-                    fontSize: "18px",
-                    fontWeight: "bold",
-                    fontStyle: "italic",
-                  }}
-                >
-                  {name}
-                </span>
-                <br />
-                Email:&nbsp;&nbsp;
-                {email}
-                <br />
-                Location:&nbsp;&nbsp;
-                {location}
-                <br />
-                Password:&nbsp;&nbsp;
-                {password}
-              </div>
-              <button type="button" className="button">
+        <div className="itemInfo">
+          <div className="sectionheader">Profile</div>
+          <div className="sectionhr"></div>
+          <table className="tableprofile">
+            <tr>
+              <th className="tableprofileitems">Name:</th>
+              <td>{name}</td>
+            </tr>
+
+            <tr>
+              <th className="tableprofileitems">Email:</th>
+              <td>{email}</td>
+            </tr>
+
+            <tr>
+              <th className="tableprofileitems">Location:</th>
+              <td>{location}</td>
+            </tr>
+
+            <tr>
+              <th className="tableprofileitems">Password:</th>
+              <td>{password}</td>
+              {/* <td>{password.replace(/./gi, "*")}</td> */}
+            </tr>
+            <div className="sectionbutton">
+              <button type="button" className="button" onClick={() => callAction("eprofile")}>
                 edit
               </button>
             </div>
-          </div>
+          </table>
         </div>
       </div>
     </>
